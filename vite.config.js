912d56e8572c5b48031319facef03792e4a46f07
victor-yakson <victoryakubu09@.com>
2024-05-15 19:45:58 +0100

    fixing crashes

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
